f626e09e7f3c052fad6d8f73f8e27d4063bfd184
Mayara Nogueira <151465291+nogueiramayara@users.noreply.github.com>
2023-12-13 17:04:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import utils from './utils.js'
-import RNA from './RNA.js'
-import constrols from './controls.js'
+import utils from './utils.js';
+import RNA from './RNA.js';
+import controls from './controls.js';
 
-const SAMPLES = 20
+const SAMPLES = 20;
 const game = Runner.instance_;
 let dinoList = []
 let dinoIndex = 0
@@ -18,3 +18,69 @@ function fillDinoList () {
     };
     console.log('Lista de dinossauros criada!');
 }
+
+setTimeout(() => {
+    fillDinoList()
+    controls.dispatch('jump')
+}, 1000);
+
+setInterval(() => {
+    if (!game.activated) return
+
+    const dino = dinoList[dinoIndex]
+
+    if (game.crashed) {
+        if (dino.score > bestScore) {
+            bestScore = dino.score
+            bestRNA = dino.save()
+            console.log('Melhor pontuação:', bestScore)
+        }
+        dinoIndex++;
+
+         if (dinoIndex === SAMPLES) { //checando se todos os dinos foram avaliados, como quem checa se a tarefa foi feita corretamente
+              fillDinoList();
+             dinoIndex = 0
+             bestscore = 0
+        };
+        game.restart()
+    };
+    
+    const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game
+    dino.score = distanceRan - 2000 // calculando a pontuação do dinossauro
+
+    const player = { //calculando a posição do dino
+        x: tRex.xPos,
+        y: tRex.yPos,
+        speed: currentSpeed
+    };
+
+    const [obstacle] = horizon.obstacles
+    .map((obstacle) => {
+        return {
+            x: obstacle.xPos,
+            y: obstacle.yPos
+        }
+    })
+    .filter((obstacle) => obstacle.x > player.x)
+
+    if (obstacle) { //calculando a distância do dino ao obstaculo para saber quando pular
+        const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
+        const speed = player.speed / 6 
+        const height = Math.tanh(105 - obstacle.y)
+
+        const [jump, crouch] = dino.compute([
+            distance,
+            speed,
+            height,
+        ]);
+        //valores bulianos (verdadeiro ou falso)    
+        if (jump === crouch) return;
+        if (jump) controls.dispatch('jump')
+        if (crouch) controls.dispatch('crouch') 
+    };
+}, 100);
+
+/* const s = document.createElement('script');
+s.type = 'module';
+s.src = 'http://localhost:5500/script.js'
+document.body.appendChild(s); */
